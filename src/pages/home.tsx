d16f6e15cf8b486f104d133af3fbfac99c5777c7
Wmeow_ <emaysupisara@gmail.com>
2023-08-04 15:31:27 +0700

    home edit

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,34 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {FiMenu} from 'react-icons/fi'
 
-export default function Home(){
-    return (
-        <header className="bg-[#EDE7D7]" >
-            <nav className="md:flex md:items-center md:justify-between p-4 ">
+const Home = () => {
+    let [open, setOpen] = useState(false);
+    return (     
+        <header className="w-full fixed top-0 left-0 " >
+            <nav className="md:flex md:items-center md:justify-between p-2 px-4 bg-[#EDE7D7]">
                 <div className='flex justify-between items-center'>
-                    <div className='flex-col'>
+                    <span className='flex-col'>
                         <p className="text-2xl font-serif text-center text-[#778773] flex-grow font-semibold " >CAROL</p>
                         <p className="-mt-1.5 text-sm text-center font-serif text-[#778773] flex-grow font-normal ">RENTAL</p> 
-                    </div>
-
-                    <span className='text-3xl cursor-pointer md:hidden block'>
+                    </span> 
+                    <span className='text-3xl cursor-pointer md:hidden block '>
                         <FiMenu/>
-                    </span>
+                    </span>       
                 </div>
+                
+    
 
-                <ul className="md:flex md:item-center z-[-1] md:z-auto md:static absoloute bg-[#EDE7D7] w-full left-0 md:w-auto md:py-0 py-4 md:pl-0
-                    p-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+                <ul className="md:flex md:item-center -mr-4 md:pb-0 pb-12 absolute md:static bg-[#EDE7D7] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-4 transition-all duration-500 ease-in">
                     <li className='mx-4 my-6 md:my-0'>
-                        <a href='' className='text-sm text-[#341F0F] hover:text-[#D25656] duration-500'>PRODUCT</a>
+                        <a href='' className='text-sm text-[#341F0F] font-serif hover:text-[#D25656] duration-300'>PRODUCT</a>
                     </li>
                     <li className='mx-4 my-6 md:my-0'>
-                        <a href='#' className='text-sm text-[#341F0F] hover:text-[#D25656] duration-500'>ABOUT US</a>
+                        <a href='#' className='text-sm text-[#341F0F] font-serif hover:text-[#D25656] duration-300'>ABOUT US</a>
                     </li>
                     <li className='mx-4 my-6 md:my-0'>
-                        <a href='#' className='text-sm text-[#341F0F] hover:text-[#D25656] duration-500'>HOW TO RENT</a>
+                        <a href='#' className='text-sm text-[#341F0F] font-serif hover:text-[#D25656] duration-300'>HOW TO RENT</a>
                     </li>
                     <li className='mx-4 my-6 md:my-0'>
-                        <a href='#' className='text-sm text-[#341F0F] hover:text-[#D25656] duration-500'>LOGIN</a>
+                        <a href='#' className='text-sm text-[#341F0F] font-serif hover:text-[#D25656] duration-300'>LOGIN</a>
                     </li>
                 </ul>
                 
@@ -37,3 +38,5 @@ export default function Home(){
         </header>
     )
 }
+
+export default Home;
