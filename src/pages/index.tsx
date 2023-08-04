57758ba866dff53a37f314eb882e5914ba9ba187
Wmeow_ <emaysupisara@gmail.com>
2023-08-04 15:32:15 +0700

    วันที่4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 export default function Home(){
     return (
       <header className="bg-[#EDE7D7] " >
@@ -11,32 +12,14 @@ export default function Home(){
               </a> 
             </div>
 
-            <div className="ml-9">
-                <div className="relative  flex w-full flex-wrap items-stretch">
-                  <input
-                    type="search"
-                    className="relative m-0 block w-[1px] min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-                    placeholder="Search"
-                    aria-label="Search"
-                    aria-describedby="button-addon2" />
-                    <span className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
-                      id="basic-addon2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="h-5 w-5">
-                        <path
-                        fill-rule="evenodd"
-                        d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                        clip-rule="evenodd" />
-                      </svg>
-                    </span>
-                  </div>
-                </div>
 
+            <div className="topnav a">
+              <input type="text" placeholder="Search..."/>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="#341f0fa2" className="bi bi-search fixed-icon md:flex items-center" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+              </svg>
+            </div>
 
-            
               
           </nav>
       </header>  
